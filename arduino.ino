9d93de40e4e15e1904b86d9852bad9b437b033cf
Marcus Kwan <themarcusmario0605@gmail.com>
2026-08-19 20:40:13 +0800

    Fixed intro pronouncation issues.

--- arduino.ino
+++ arduino.ino
@@ -1,4 +1,4 @@
-// For ESP32 Firebeetle (2026-08-12)
+// Code for ESP32 Firebeetle (2026-08-19)
 
 #include <PWMOutESP32.h>
 #include <Wire.h>  // Arduino IDE built-in
@@ -12,11 +12,12 @@
 #define SCK 4     // Serial Clock (SCK) pin
 #define MOSI 17   // Master Output Slave Input (MOSI) pin
 #define CS_PIN 16
+#define LED_SPD 18 // Scrolling speed (pixels/second)
 
 // Defining the ports for LCD (I2C).
 #define I2C_SDA 21
 #define I2C_SCL 22
-#define MAX_DISPLAY  6
+#define MAX_DISPLAY  8
 
 // Defining the key and debounce ratae for button detection.
 #define MON 5
@@ -41,7 +42,10 @@ byte display = -1;
 unsigned long trainMessageTimer = 0;
 int stationIndex = 0;
 bool trainSequenceActive = false;
-const unsigned long TRAIN_MESSAGE_INTERVAL = 18500;
+const unsigned long TRAIN_MESSAGE_INTERVAL = 18000; // Set delay time (in ms) between each LED message.
+
+const int* activeStationRoute = nullptr;
+int activeStationRouteLength = 0;
 
 // For LCD display sequence
 unsigned long lcdMessageTimer = 0;
@@ -50,7 +54,7 @@ bool localExpBlueActive = false;
 String lcdRouteTitle;
 String lcdRouteOrigin;
 String lcdRouteDestination;
-const unsigned long LCD_MESSAGE_INTERVAL = 3000;
+const unsigned long LCD_MESSAGE_INTERVAL = 3000; // Set delay time (in ms) between each LCD info display.
 
 // Button Debounce
 void IRAM_ATTR mon_pressed() {
@@ -65,13 +69,29 @@ void IRAM_ATTR mon_pressed() {
 
 // LEDs Initialize (PWM)
 void initializeLed() {
-  pwm.analogWrite(RED, MIN_RESOLUTION / 2);
-  pwm.analogWrite(GRN, MIN_RESOLUTION / 2);
-  pwm.analogWrite(BLUE, MIN_RESOLUTION / 2);
+  pwm.analogWrite(RED, MIN_RESOLUTION*2);
+  pwm.analogWrite(GRN, MIN_RESOLUTION*2);
+  pwm.analogWrite(BLUE, MIN_RESOLUTION*2);
+}
+
+void localLED() {
+  pwm.analogWrite(RED, MAX_RESOLUTION);
+  pwm.analogWrite(GRN, MAX_RESOLUTION);
+  pwm.analogWrite(BLUE, 1540);
+}
+void expressLED() {
+  pwm.analogWrite(RED, MIN_RESOLUTION);
+  pwm.analogWrite(GRN, 3840);
+  pwm.analogWrite(BLUE, MAX_RESOLUTION);
+}
+void rapidLED() {
+  pwm.analogWrite(RED, MIN_RESOLUTION);
+  pwm.analogWrite(GRN, 2840);
+  pwm.analogWrite(BLUE, MAX_RESOLUTION);
 }
 
 void setup() {
-  delay(15); //wait around 15ms until power is stable during start-up
+  delay(15); // wait around 15ms until power is stable during start-up
 
   // Initialize the LED matrix
   mtrx.begin();
@@ -105,7 +125,7 @@ void setup() {
 
 const char* stations[] = { 
                         // Odakyu Main Line
-                        "Minami-Shinjuku (OH-02)", "Sangubashi (OH-03)", "Yoyogi-Hachiman (OH-04)",
+                        "Shinjuku (OH-01)", "Minami-Shinjuku (OH-02)", "Sangubashi (OH-03)", "Yoyogi-Hachiman (OH-04)",
                         "Yoyogi-Uehara (OH-05)", "Higashi-Kitazawa (OH-06)", "Shimo-Kitazawa (OH-07)", "Setagaya-Daita (OH-08)", 
                         "Umegaoka (OH-09)", "Gotokuji (OH-10)", "Kyodo (OH-11)", "Chitose-Funabashi (OH-12)", 
                         "Soshigaya-Okura (OH-13)", "Seijogakuen-mae (OH-14)", "Kitami (OH-15)", "Komae (OH-16)", 
@@ -118,46 +138,48 @@ const char* stations[] = {
                         "Shin-Matsuda (OH-41)", "Kaisei (OH-42)", "Kayama (OH-43)", "Tomizu (OH-44)", 
                         "Hotaruda (OH-45)", "Ashigara (OH-46)", "Odawara (OH-47)",
 
-                        // Tama Line (From Shin-Yurigaoka OH-23) [idx: 46-52]
+                        // Tama Line (From Shin-Yurigaoka OH-23) [idx: 47-53]
                         "Satsukidai (OT-01)", "Kurihira (OT-02)", "Kurokawa (OT-03)", "Haruhino (OT-04)",
                         "Odakyu-Nagayama (OT-05)", "Odakyu-Tama-Center (OT-06)", "Karakida (OT-07)",
                         
-                        // Enoshima Line (From Sagami-Ōno OH-28) [idx: 53-68]
+                        // Enoshima Line (From Sagami-Ono OH-28) [idx: 54-69]
                         "Higashi-Rinkan (OE-01)", "Chuo-Rinkan (OE-02)", "Minami-Rinkan (OE-03)", "Tsuruma (OE-04)",
                         "Yamato (OE-05)", "Sakuragaoka (OE-06)", "Koza-Shibuya (OE-07)", "Chogo (OE-08)",
                         "Shonandai (OE-09)", "Mutsuai-Nichidaimae (OE-10)", "Zengyo (OE-11)", "Fujisawa-Hommachi (OE-12)",
                         "Fujisawa (OE-13)", "Hon-Kugenuma (OE-14)", "Kugenuma-Kaigan (OE-15)", "Katase-Enoshima (OE-16)",
-
-                        // Upper bound Terminal [idx: 69]
-                        "Shinjuku (OH-01)",
                         };
 
 // From Shinjuku (OH-01) to Hon-Atsugi (OH-34)
-const int localStation1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
+const int localStation1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33};
 const int localStation1Length = sizeof(localStation1) / sizeof(localStation1[0]);
 
+// From Shin-Yurigaoka (OH-23) to Karakida (OT-07)
+const int localStation2[] = {47, 48, 49, 50, 51, 52, 53};
+const int localStation2Length = sizeof(localStation2) / sizeof(localStation2[0]);
+
+// From Sagami-Ono (OH-28) to Katase-Enoshima (OE-16)
+const int localStation3[] = {54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69};
+const int localStation3Length = sizeof(localStation3) / sizeof(localStation3[0]);
+
 // From [EXP] Machida (OH-27) to Shin-Matsuda (OH-41) and [LOC] to Odawara (OH-47)
-const int localExpStation1[] = {25, 26, 30, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45};
+const int localExpStation1[] = {27, 31, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46};
 const int localExpStation1Length = sizeof(localExpStation1) / sizeof(localExpStation1[0]);
 
 // From Shinjuku (OH-01) to Shin-Yurigaoka (OH-23) and *through service* to Karakida (OT-07)
-const int expressStation1[] = {3, 5, 9, 12, 16, 17, 21, 46, 47, 48, 49, 50, 51, 52};
+const int expressStation1[] = {4, 6, 10, 13, 17, 18, 22, 47, 48, 49, 50, 51, 52, 53};
 const int expressStation1Length = sizeof(expressStation1) / sizeof(expressStation1[0]);
 
 // From Machida (OH-27) to Shinjuku (OH-01) (UPPER BOUND)
-const int expressStation2[] = {3, 5, 9, 12, 16, 17, 21, 17, 16, 12, 9, 5, 3, 69};
+const int expressStation2[] = {22, 18, 17, 13, 10, 6, 4, 0};
 const int expressStation2Length = sizeof(expressStation2) / sizeof(expressStation2[0]);
 
 // From Shinjuku (OH-01) to Odawara (OH-47)
-const int rapidexStation1[] = {3, 5, 16, 21, 25, 26, 30, 32, 33, 34, 35, 36, 37, 38, 39, 40, 45};
+const int rapidexStation1[] = {4, 6, 17, 22, 26, 27, 31, 33, 34, 35, 36, 37, 38, 39, 40, 41, 46};
 const int rapidexStation1Length = sizeof(rapidexStation1) / sizeof(rapidexStation1[0]);
 
 // From Shinjuku (OH-01) to Sagami-Ono (OH-28) and *through service* to Fujisawa (OE-13)
-const int rapidexStation2[] = {3, 5, 16, 21, 25, 26, 54, 57, 61, 65}; 
+const int rapidexStation2[] = {4, 6, 17, 22, 26, 27, 55, 58, 62, 66}; 
 const int rapidexStation2Length = sizeof(rapidexStation2) / sizeof(rapidexStation2[0]);
-
-const int* activeStationRoute = nullptr;
-int activeStationRouteLength = 0;
 
 void startTrainSequence(const int* route, int routeLength, String introMessage) {
   localExpBlueActive = false;
@@ -172,27 +194,43 @@ void startTrainSequence(const int* route, int routeLength, String introMessage) 
 }
 
 void startLocal1TrainSequence() {
+  localLED();
   startTrainSequence(localStation1, localStation1Length, String("The local train bound for Hon-Atsugi.            "));
 }
 
+void startLocal2TrainSequence() {
+  localLED();
+  startTrainSequence(localStation2, localStation2Length, String("The local train bound for Karakida.            "));
+}
+
+void startLocal3TrainSequence() {
+  localLED();
+  startTrainSequence(localStation3, localStation3Length, String("The local train bound for Katase-Enoshima.            "));
+}
+
 void startLocalExp1TrainSequence() {
-  startTrainSequence(localExpStation1, localExpStation1Length, String("The express train bound for Odawara.            "));
+  expressLED();
+  startTrainSequence(localExpStation1, localExpStation1Length, String("The express bound for Odawara.            "));
 }
 
 void startExpress1TrainSequence() {
-  startTrainSequence(expressStation1, expressStation1Length, String("The express train bound for Karakida.            "));
+  expressLED();
+  startTrainSequence(expressStation1, expressStation1Length, String("The express bound for Karakida.            "));
 }
 
 void startExpress2TrainSequence() {
-  startTrainSequence(expressStation2, expressStation2Length, String("The express train bound for Shinjuku.            "));
+  expressLED();
+  startTrainSequence(expressStation2, expressStation2Length, String("The express bound for Shinjuku.            "));
 }
 
 void startRapid1TrainSequence() {
-  startTrainSequence(rapidexStation1, rapidexStation1Length, String("The rapid train bound for Odawara.            "));
+  rapidLED();
+  startTrainSequence(rapidexStation1, rapidexStation1Length, String("The rapid-express bound for Odawara.            "));
 }
 
 void startRapid2TrainSequence() {
-  startTrainSequence(rapidexStation2, rapidexStation2Length, String("The rapid train bound for Fujisawa.            "));
+  rapidLED();
+  startTrainSequence(rapidexStation2, rapidexStation2Length, String("The rapid-express bound for Fujisawa.            "));
 }
 
 void restartActiveTrainSequence() {
@@ -221,15 +259,15 @@ void updateTrainSequence() {
   }
 
   if (stationIndex >= activeStationRouteLength) {
+    initializeLed();
+    delay(100);
     restartActiveTrainSequence();
     return;
   }
 
   // Specifically for the Local Express route
   if (activeStationRoute == localExpStation1 && !localExpBlueActive && activeStationRoute[stationIndex] == 39) {
-    pwm.analogWrite(RED, MAX_RESOLUTION);
-    pwm.analogWrite(GRN, MAX_RESOLUTION);
-    pwm.analogWrite(BLUE, 1540);
+    localLED();
     localExpBlueActive = true;
   }
 
@@ -282,101 +320,65 @@ void loop() {
     
     switch (display) {
       case 0:
-        pwm.analogWrite(RED, MAX_RESOLUTION);
-        pwm.analogWrite(GRN, MAX_RESOLUTION);
-        pwm.analogWrite(BLUE, 1540);
-
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [LOCAL]");
-        setRouteLcd("Odakyu [LOCAL]", "From: Shinjuku", "For: Hon-Atsugi");
-        updateRouteLcd();
+        setRouteLcd("Odakyu [LOCAL]", "FROM: Shinjuku", "TO: Hon-Atsugi");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startLocal1TrainSequence();
         break;
       case 1:
-        pwm.analogWrite(RED, MAX_RESOLUTION);
-        pwm.analogWrite(GRN, MAX_RESOLUTION);
-        pwm.analogWrite(BLUE, 1540);
-
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [EXP>LOC]");
-        setRouteLcd("Odakyu [EXP>LOC]", "From: Machida", "For: Odawara");
-        updateRouteLcd();
+        setRouteLcd("Odakyu Tama", "FROM: Shin-Yuri.", "TO: Karakida");
+
+        attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
+        startLocal2TrainSequence();
+        break;
+      case 2:
+        detachInterrupt(digitalPinToInterrupt(MON));
+
+        setRouteLcd("Odakyu Enoshima", "FROM: Sagami-Ono", "TO: Katase-Eno.");
+
+        attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
+        startLocal3TrainSequence();
+        break;
+      case 3:
+        detachInterrupt(digitalPinToInterrupt(MON));
+
+        setRouteLcd("Odakyu [EXP>LOC]", "FROM: Machida", "TO: Odawara");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startLocalExp1TrainSequence();
         break;
-      case 2:
-        pwm.analogWrite(RED, MIN_RESOLUTION);
-        pwm.analogWrite(GRN, 3840);
-        pwm.analogWrite(BLUE, MAX_RESOLUTION);
-
+      case 4:
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [EXPRESS]");
-        setRouteLcd("Odakyu [EXPRESS]", "From: Shinjuku", "For: Shin-Yuri.");
-        updateRouteLcd();
+        setRouteLcd("Odakyu [EXPRESS]", "FROM: Shinjuku", "TO: Karakida");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startExpress1TrainSequence();
         break;
-      case 3:
-        pwm.analogWrite(RED, MIN_RESOLUTION);
-        pwm.analogWrite(GRN, 3840);
-        pwm.analogWrite(BLUE, MAX_RESOLUTION);
-
+      case 5:
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [EXPRESS]");
-        setRouteLcd("Odakyu [EXPRESS]", "From: Machida", "For: Shinjuku");
-        updateRouteLcd();
+        setRouteLcd("Odakyu [EXPRESS]", "FROM: Machida", "TO: Shinjuku");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startExpress2TrainSequence();
         break;
-      case 4:
-        pwm.analogWrite(RED, MIN_RESOLUTION);
-        pwm.analogWrite(GRN, 2840);
-        pwm.analogWrite(BLUE, MAX_RESOLUTION);
-
-        digitalWrite(RED, LOW);
-        digitalWrite(GRN, LOW);
-        digitalWrite(BLUE, HIGH);
-
+      case 6:
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [RAPID]");
-        setRouteLcd("Odakyu [RAPID]", "From: Shinjuku", "For: Odawara");
-        updateRouteLcd();
+        setRouteLcd("Odakyu [RAPID]", "FROM: Shinjuku", "TO: Odawara");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startRapid1TrainSequence();
         break;
-      case 5:
-        pwm.analogWrite(RED, MIN_RESOLUTION);
-        pwm.analogWrite(GRN, 2840);
-        pwm.analogWrite(BLUE, MAX_RESOLUTION);
-
+      case 7:
         detachInterrupt(digitalPinToInterrupt(MON));
 
-        lcd.clear(); // Clear the content in LCD.
-        lcd.setCursor(0,0);
-        lcd.print("Odakyu [RAPID]");
-        setRouteLcd("Odakyu [RAPID]", "From: Shinjuku", "For: Sagami-Ono");
-        updateRouteLcd();
+        setRouteLcd("Odakyu [RAPID]", "FROM: Shinjuku", "TO: Fujisawa");
 
         attachInterrupt(digitalPinToInterrupt(MON),mon_pressed,RISING);
         startRapid2TrainSequence();
